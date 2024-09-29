308f90ac4d5c1baf1d879e57942a034b2f5a6d76
Mr1-D3CRYPT <ashishthomas06@gmail.com>
2024-09-29 13:35:39 +0530

    last

--- storyproject/settings.py
+++ storyproject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,16 +79,20 @@ WSGI_APPLICATION = 'storyproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kathakosh',
-        'USER': 'postgres',
-        'PASSWORD': 'AaroIndian',
-        'HOST': 'localhost',   
-        'PORT': '5432',
-    }
+    'default' : dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+##DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+       # 'ENGINE': 'django.db.backends.postgresql',
+      #  'NAME': 'kathakosh',
+     #   'USER': 'postgres',
+    #    'PASSWORD': 'AaroIndian',
+   #     'HOST': 'localhost',   
+  #      'PORT': '5432',
+ #   }
+#}
 
 
 
